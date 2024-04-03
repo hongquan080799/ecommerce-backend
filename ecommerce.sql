fe5037f594c87d9288964ea84c48acc95cce1230
hongquan <hongquan080799@gmail.com>
2024-04-04 00:32:39 +0700

    add api category

--- ecommerce.sql
+++ ecommerce.sql
@@ -5,3 +5,11 @@ use ecommerce;
 create table category (
     id bigint primary key auto_increment, name varchar(500), image_url text, parent_id bigint
 );
+
+ALTER TABLE category
+ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ADD COLUMN created_by VARCHAR(255) DEFAULT NULL,
+ADD COLUMN updated_by VARCHAR(255) DEFAULT NULL;
+
+select * from category;
